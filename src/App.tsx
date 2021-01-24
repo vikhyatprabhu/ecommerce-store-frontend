166aab48324d114ccdbfa6e2abb9a37fedd6234e
vikhyat.prabhu <vikhyat.prabhu@gmail.com>
2021-01-24 14:45:51 +0530

    Add search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import { Button, LinearProgress } from "@material-ui/core";
+import {
+  Button,
+  IconButton,
+  InputAdornment,
+  LinearProgress,
+  TextField,
+} from "@material-ui/core";
 import { useEffect, useState } from "react";
-
+import SearchIcon from "@material-ui/icons/Search";
 import BookCard from "./components/BookCard/BookCard";
 import { Book } from "./models/models";
 import Grid from "@material-ui/core/Grid";
@@ -12,34 +18,75 @@ function App() {
   let [isLoading, setLoading] = useState(true);
   let [isError, setError] = useState(false);
   let [ratingSortFilter, setRatingSortFilter] = useState<string>("");
-  let [currentPage , setCurrentPage] = useState<number>(0);
-  let [hasPages , setHasPages] = useState(false);
-  const handleSortFilterChange = (
-    event: React.ChangeEvent<{ value: unknown }>
-  ) => {
-    setRatingSortFilter(event.target.value as string);
-  };
+  let [currentPage, setCurrentPage] = useState<number>(0);
+  let [hasPages, setHasPages] = useState(false);
+  let [searchText, setSearchText] = useState<string>("");
 
-  const handleFetchMore= async () =>{
-    try{
-    let nextPage = currentPage+1;
-    let apiOutput = await fetch("https://shrouded-crag-39023.herokuapp.com/api/v1/books?page="+nextPage); 
-    let data = await apiOutput.json();
-    console.log(data);
-    let currentBooks: Book[] = data.content;
-    setHasPages(!data.last);
-    setCurrentPage(data.number);
-    setBooks([...books,...currentBooks]);
+  const handleFetchMore = async () => {
+    try {
+      let nextPage = currentPage + 1;
+      let apiOutput = await fetch(
+       buildURLFromState() +
+          "&page=" +
+          nextPage 
+      );
+      let data = await apiOutput.json();
+      let currentBooks: Book[] = data.content;
+      setHasPages(!data.last);
+      setCurrentPage(data.number);
+      setBooks([...books, ...currentBooks]);
     } catch {
       setLoading(false);
       setError(true);
     }
+  };
+
+  const handleSearch = async () =>{
+    setLoading(true)
+    let apiOutput = await fetch(buildURLFromState());
+    console.log(apiOutput);
+    let data = await apiOutput.json();
+    console.log(data);
+    let currentBooks: Book[] = data.content;
+    setLoading(false);
+    setHasPages(!data.last);
+    setCurrentPage(Number(data.number));
+    setBooks(currentBooks);
   }
+
+  const buildURLFromState = () =>{
+    let apiUrl = getURL()
+    if(searchText !== ""){
+      apiUrl= apiUrl+"/search?title="+searchText
+    } else {
+      apiUrl= apiUrl+"?"
+    }
+    if(ratingSortFilter !==""){
+      apiUrl = apiUrl + "&sort="+ratingSortFilter
+    }
+    return apiUrl;
+  }
+  useEffect(() => {
+    const fetchSortedData = async () => {
+      let apiOutput = await fetch(buildURLFromState());
+      console.log(apiOutput);
+      let data = await apiOutput.json();
+      console.log(data);
+      let currentBooks: Book[] = data.content;
+      setLoading(false);
+      setHasPages(!data.last);
+      setCurrentPage(Number(data.number));
+      setBooks(currentBooks);
+    };
+    setLoading(true);
+    fetchSortedData();
+  }, [ratingSortFilter]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        let apiOutput = await fetch("https://shrouded-crag-39023.herokuapp.com/api/v1/books");
+       
+        let apiOutput = await fetch(getURL());
         console.log(apiOutput);
         let data = await apiOutput.json();
         console.log(data);
@@ -63,11 +110,31 @@ function App() {
   return (
     <div className="App">
       <Grid container direction={"row"} justify="center" spacing={10}>
-        <Grid item>
-          <SortByRating
-            ratingSortFilter={ratingSortFilter}
-            handleSortFilterChange={handleSortFilterChange}
-          />
+        <Grid item container>
+          <Grid item xs={4} sm={4}>
+            <TextField
+              label="Search"
+              value={searchText}
+              onChange = {(event) => setSearchText(event.target.value as string)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleSearch} >
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <SortByRating
+              ratingSortFilter={ratingSortFilter}
+              handleSortFilterChange={(event) =>
+                setRatingSortFilter(event.target.value as string)
+              }
+            />
+          </Grid>
         </Grid>
         <Grid>
           <Grid item container spacing={1}>
@@ -79,7 +146,13 @@ function App() {
           </Grid>
         </Grid>
         <Grid item>
-          <Button disabled={!hasPages} onClick={handleFetchMore} size="large" variant="contained" color="primary">
+          <Button
+            disabled={!hasPages}
+            onClick={handleFetchMore}
+            size="large"
+            variant="contained"
+            color="primary"
+          >
             Load More
           </Button>
         </Grid>
@@ -89,3 +162,11 @@ function App() {
 }
 
 export default App;
+function getURL() {
+  let url = "http://localhost:8080";
+  if (process.env.REACT_APP_BOOKSTORE_SERVICE) {
+    url = process.env.REACT_APP_BOOKSTORE_SERVICE;
+  }
+  return url;
+}
+
--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -6,9 +6,9 @@ import {
   createStyles,
   makeStyles,
   Theme,
-  Typography
+  Typography,
 } from "@material-ui/core";
-import StarsIcon from '@material-ui/icons/Stars';
+import StarsIcon from "@material-ui/icons/Stars";
 import React from "react";
 import { Book } from "../../models/models";
 
@@ -16,28 +16,32 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       maxWidth: 500,
-      height:'100%' 
+      height: "100%",
     },
     media: {
       height: 100,
-      paddingTop: '56.25%', // 16:9
-    }
-  }));
-
+      paddingTop: "56.25%", // 16:9
+    },
+  })
+);
 
 const BookCard: React.FC<BookProps> = ({ book }) => {
   const classes = useStyles();
   return (
-    <Card className= {classes.root}>
-      <CardHeader
+    <Card className={classes.root}>
+      <CardHeader title={book.title} subheader={book.authors} />
+      <CardMedia
+        className={classes.media}
+        image={book.imageUrl}
         title={book.title}
-        subheader={book.authors}
       />
-      <CardMedia className={classes.media} image={book.imageUrl} title={book.title} />
       <CardContent>
-        <span>{Math.round(book?.average_rating*10)/10}<StarsIcon/> ( {book.ratings_count} ) </span>
+        <span>
+          {Math.round(book?.average_rating * 10) / 10}
+          <StarsIcon /> ( {book.ratings_count} ){" "}
+        </span>
         <Typography variant="body2" color="textSecondary" component="p">
-         {"Price : Rs."+ book.price}
+          {"Price : Rs." + book.price}
         </Typography>
       </CardContent>
     </Card>
