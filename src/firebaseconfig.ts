2cec80b7d3e292ef8e23ab5fc06c18ffb1260d02
vikhyat.prabhu <vikhyat.prabhu@gmail.com>
2021-01-24 17:44:52 +0530

    Add fire base config

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCCDdAUMhBUSLC1SA__PKu3Ndn1CEyK7WA",
+    authDomain: "ecommerce-checkout-3445c.firebaseapp.com",
+    projectId: "ecommerce-checkout-3445c",
+    storageBucket: "ecommerce-checkout-3445c.appspot.com",
+    messagingSenderId: "89169155060",
+    appId: "1:89169155060:web:c7255b9f33afccd5c75fa1",
+    measurementId: "G-N3HEXEQ0LT"
+  };
+
+export default firebaseConfig;
